f457b1c0247917acbdd768e9d061957c0fba2a9a
Alexandr Ushakov <al.ushakov@okko.tv>
2025-04-09 14:33:08 +0300

    fix npm pkg name

--- provider/resources.go
+++ provider/resources.go
@@ -140,6 +140,7 @@ func Provider() tfbridge.ProviderInfo {
 
 		JavaScript: &tfbridge.JavaScriptInfo{
 			// RespectSchemaVersion ensures the SDK is generated linking to the correct version of the provider.
+                        PackageName:          "pulumi-sbercloud",
 			RespectSchemaVersion: true,
 		},
 		Python: &tfbridge.PythonInfo{
